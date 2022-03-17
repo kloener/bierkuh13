c392e5dae8c48536e9261e0ad81ed07fa1d573db
Christian Illies <github@christianillies.de>
2022-03-17 21:08:36 +0100

    Add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bierkuh-ef2d6',
+    appId: '1:51217565024:web:9b9dd18da29b6ed2df8a05',
+    databaseURL: 'https://bierkuh-ef2d6-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'bierkuh-ef2d6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAClC4XPJTCzX2dwP3QY7r8zcGRLzwA5xM',
+    authDomain: 'bierkuh-ef2d6.firebaseapp.com',
+    messagingSenderId: '51217565024',
+  },
   production: false
 };
 
