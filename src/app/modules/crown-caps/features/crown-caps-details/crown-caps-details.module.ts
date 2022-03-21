2c6667ad4008b819332b6963c7afe47cf593e980
c.illies <c.illies@finatix.de>
2022-03-21 20:08:15 +0100

    Detail page infos added

--- src/app/modules/crown-caps/features/crown-caps-details/crown-caps-details.module.ts
+++ src/app/modules/crown-caps/features/crown-caps-details/crown-caps-details.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CapImgModule } from '@app/shared/cap-img/cap-img.module';
 
 import { CrownCapsDetailsComponent } from './crown-caps-details.component';
 
 @NgModule({
   declarations: [CrownCapsDetailsComponent],
-  imports: [CommonModule, CapImgModule],
+  imports: [CommonModule, RouterModule, CapImgModule],
   exports: [CrownCapsDetailsComponent],
 })
 export class CrownCapsDetailsModule {}
