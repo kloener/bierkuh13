5770765532bf3fbe468248c6779c93930a7c5137
c.illies <c.illies@finatix.de>
2022-03-21 13:48:41 +0100

    Add details method to find crown object

--- src/app/modules/crown-caps/application/crown-caps-details-facade.service.ts
+++ src/app/modules/crown-caps/application/crown-caps-details-facade.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { CrownCapsListFacadeService } from './crown-caps-list-facade.service';
+import { map, Observable } from 'rxjs';
+import { CrownCaps } from '../domain/crown-caps';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrownCapsDetailsFacadeService {
-
-  constructor() { }
+  constructor(private readonly listFacadeService: CrownCapsListFacadeService) {}
+  getDetailsOf(identifier: string): Observable<CrownCaps | undefined> {
+    return this.listFacadeService.allCaps$.pipe(map(list => list.find(item => item.name === identifier)));
+  }
 }
