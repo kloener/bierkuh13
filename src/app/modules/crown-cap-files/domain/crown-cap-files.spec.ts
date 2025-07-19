ce3e8034e6a8f3aa1caa6def54d44626ce0e9b9f
Christian Illies (FINATIX) <c.illies@finatix.de>
2025-07-19 13:29:55 +0200

    chore: remove all tests

--- src/app/modules/crown-cap-files/domain/crown-cap-files.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CrownCapFiles } from './crown-cap-files';
-
-describe('CrownCapFiles', () => {
-  it('should create an instance', () => {
-    expect(new CrownCapFiles()).toBeTruthy();
-  });
-});
