e4ec940948714bc7e9d5ad145ee7097332227f11
Christian Illies <post@christianillies.de>
2025-07-20 15:52:58 +0200

    fix: add RouterLinkActive to header

--- src/app/shared/bierkuh-header/bierkuh-header.component.ts
+++ src/app/shared/bierkuh-header/bierkuh-header.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule, RouterLink } from '@angular/router';
+import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-bierkuh-header',
   templateUrl: './bierkuh-header.component.html',
   styleUrls: ['./bierkuh-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink]
+  imports: [RouterLink, RouterLinkActive]
 })
 export class BierkuhHeaderComponent {
 
